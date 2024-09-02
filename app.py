f09bde44a1e96d83c8046b24cd1412c1adbd6a41
Peter Dalwoo Kim <xerac318@gmail.com>
2024-09-03 02:07:17 +0900

    Initial commit with Flask finance tracker

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///finance.db'
+db = SQLAlchemy(app)
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(10), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(100), nullable=True)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():
+    transactions = Transaction.query.all()
+    return render_template('index.html', transactions=transactions)
+
+@app.route('/add', methods=['POST'])
+def add_transaction():
+    date = request.form['date']
+    amount = request.form['amount']
+    category = request.form['category']
+    description = request.form['description']
+
+    new_transaction = Transaction(date=date, amount=float(amount), category=category, description=description)
+    db.session.add(new_transaction)
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:id>')
+def delete_transaction(id):
+    transaction = Transaction.query.get(id)
+    db.session.delete(transaction)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
