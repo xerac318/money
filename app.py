123d00f3eae77b80698e6e5a42a2b0dcf3cbb27f
Peter Dalwoo Kim <xerac318@gmail.com>
2024-09-03 02:50:29 +0900

    Updated to use PostgreSQL

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///finance.db'
+
+# PostgreSQL 데이터베이스 URI 설정
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://moneybook_db_user:CBaBtRfohayVn36bNUUyQflS5sqbyxD5@dpg-cravkg56l47c73d1cm8g-a.singapore-postgres.render.com/moneybook_db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
+# Transaction 모델 정의
 class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.String(10), nullable=False)
@@ -12,8 +18,8 @@ class Transaction(db.Model):
     category = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(100), nullable=True)
 
-with app.app_context():
-    db.create_all()
+# 데이터베이스 테이블 생성
+db.create_all()
 
 @app.route('/')
 def index():
